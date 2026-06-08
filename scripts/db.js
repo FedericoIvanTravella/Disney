110258698c2ed41d1f2201e19737c2103eb7709f
Sol Agustina Gutierrez Cao <sol.gutierrezcao@gmail.com>
2026-06-08 14:26:57 -0300

    8/6

---
+++ scripts/db.js
@@ -0,0 +1,10 @@
+export const DB=[
+    {
+        ID:1,
+        Nombre:'Funko Pop House Of The Dragon',
+        Precio:63000,
+        Stock:10,
+        Descripcion:'Aemond Targaryen',    
+        Imagen:'../src/productos/1.png'
+    },
+]
