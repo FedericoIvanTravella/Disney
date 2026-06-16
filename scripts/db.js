3ec8495d26d3dae40dbc67dc7963202acd885f95
Sol Agustina Gutierrez Cao <sol.gutierrezcao@gmail.com>
2026-06-15 22:14:19 -0300

    15/6

--- scripts/db.js
+++ scripts/db.js
@@ -1,162 +1,174 @@
+
 export const DB=[
     {
         ID:1,
         Nombre:'Peluche de Mickey Mouse',
         Precio:63000,
-        categoria:'Peluches',
+        Categoria:'Peluches',
+        Tipo: 'disney',
         Descripcion:'Peluche de 30cm',    
-        Imagen:'../src/productos/1.jpg'
+        Imagen:'../src/Productos/Productos/1.jpg'
     },
     {
         ID:2,
         Nombre:'Peluche de Minnie Mouse',
         Precio:55000,
-        categoria:'Peluches',
+        Categoria:'Peluches',
+        Tipo: 'disney',
         Descripcion:'Peluche de 25cm',
-        Imagen:'../src/productos/2.jpg'
+        Imagen:'../src/Productos/Productos/2.jpg'
     },
     {
         ID:3,
         Nombre:'Peluche de Stitch',
         Precio:45000,
-        categoria:'Peluches',
+        Categoria:'Peluches',
+        Tipo: 'disney',
         Descripcion:'Peluche de 20cm',
-        Imagen:'../src/productos/3.jpg'
+        Imagen:'../src/Productos/Productos/3.jpg'
     },
     {
         ID:4,
         Nombre:'Peluche de Winne the Pooh',
         Precio:50000,
-        categoria:'Peluches',
+        Categoria:'Peluches',
+        Tipo: 'disney',
         Descripcion:'Peluche de 30cm',
-        Imagen:'../src/productos/4.jpg'
+        Imagen:'../src/Productos/Productos/4.jpg'
     },
     {
         ID:5,
         Nombre:'Muñeca Elsa',
         Precio:55000,
-        categoria:'Figuras',
+        Categoria:'Figuras',
+        Tipo: 'disney',
         Descripcion:'Muñeca de 25cm',
-        Imagen:'../src/productos/5.jpg'
+        Imagen:'../src/Productos/Productos/5.jpg'
     },
     {
         ID:6,
         Nombre:'Muñeca Anna',
         Precio:55000,
-        categoria:'Figuras',
+        Categoria:'Figuras',
+        Tipo: 'disney',
         Descripcion:'Muñeca de 20cm',
-        Imagen:'../src/productos/6.jpg'
+        Imagen:'../src/Productos/Productos/6.jpg'
     },
     {
         ID:6,
         Nombre:'Muñeca Rapunzel',
         Precio:45000,
-        categoria:'Figuras',
+        Categoria:'Figuras',
+        Tipo: 'disney',
         Descripcion:'Muñeca de 20cm',
-        Imagen:'../src/productos/7.jpg'
+        Imagen:'../src/Productos/Productos/7.jpg'
     },
     {
         ID:6,
         Nombre:'Muñeca La Sirenita',
         Precio:45000,
-        categoria:'Figuras',
+        Categoria:'Figuras',
+        Tipo: 'disney',
         Descripcion:'Muñeca de 20cm',
-        Imagen:'../src/productos/8.jpg'
+        Imagen:'../src/Productos/Productos/8.jpg'
     },
     {
         ID:6,
         Nombre:'Muñeco de Buzz Lightyear',
         Precio:45000,
-        categoria:'Figuras',
+        Categoria:'Figuras',
+        Tipo: 'pixar',
         Descripcion:'Muñeco de 20cm',
-        Imagen:'../src/productos/9.jpg'
+        Imagen:'../src/Productos/Productos/9.jpg'
     },
     {
         ID:6,
         Nombre:'Muñeco de Woody',
         Precio:45000,
-        categoria:'Figuras',
+        Categoria:'Figuras',
+        Tipo: 'pixar',
         Descripcion:'Muñeco de 20cm',
-        Imagen:'../src/productos/10.jpg'
-    },
-    {
-        ID:6,
-        Nombre:'Muñeco de Spiderman',
-        Precio:45000,
-        categoria:'Figuras',
-        Descripcion:'Muñeco de 20cm',
-        Imagen:'../src/productos/11.jpg'
+        Imagen:'../src/Productos/Productos/10.jpg'
     },
     {
         ID:6,
         Nombre:'Muñeco de Ironman',
         Precio:45000,
-        categoria:'Figuras',
+        Categoria:'Figuras',
+        Tipo: 'marvel',
         Descripcion:'Muñeco de 25cm',
-        Imagen:'../src/productos/12.jpg'
+        Imagen:'../src/Productos/Productos/12.jpg'
     },
     {
         ID:6,
         Nombre:'Muñeco de Darth Vadar',
         Precio:45000,
-        categoria:'Figuras',
+        Categoria:'Figuras',
+        Tipo: 'starwars',
         Descripcion:'Muñeco de 30cm',
-        Imagen:'../src/productos/13.jpg'
+        Imagen:'../src/Productos/Productos/13.jpg'
     },
     {
         ID:6,
         Nombre:'Muñeco de Yoda',
         Precio:45000,
-        categoria:'Figuras',
+        Categoria:'Figuras',
+        Tipo: 'starwars',
         Descripcion:'Muñeco de 60cm',
-        Imagen:'../src/productos/14.jpg'
+        Imagen:'../src/Productos/Productos/14.jpg'
     },
     {
         ID:6,
         Nombre:'Mochila de La Sirenita',
         Precio:45000,
-        categoria:'Mochilas',
+        Categoria:'Mochilas',
+        Tipo: 'disney',
         Descripcion:'Mochila rosa con dibujos de La Sirenita',
-        Imagen:'../src/productos/15.jpg'
+        Imagen:'../src/Productos/Productos/15.jpg'
     },
     {
         ID:6,
         Nombre:'Mochila de Stitch',
         Precio:45000,
-        categoria:'Mochilas',
+        Categoria:'Mochilas',
+        Tipo: 'disney',
         Descripcion:'Mochila negra de Stitch',
-        Imagen:'../src/productos/16.png'
+        Imagen:'../src/Productos/Productos/16.png'
     },
     {
         ID:6,
         Nombre:'Mochila de Mickey Mouse',
         Precio:45000,
-        categoria:'Mochilas',
+        Categoria:'Mochilas',
+        Tipo: 'disney',
         Descripcion:'Mochila roja y negra con dibujos de mickey mouse',
-        Imagen:'../src/productos/17.jpg'
+        Imagen:'../src/Productos/Productos/17.jpg'
     },
     {
         ID:6,
         Nombre:'Taza de Mickey & Minnie',
         Precio:45000,
-        categoria:'Tazas',
+        Categoria:'Tazas',
+        Tipo: 'disney',
         Descripcion:'Taza de cerámica con dibujos de Mickey y Minnie Mouse',
-        Imagen:'../src/productos/18.jpg'
+        Imagen:'../src/Productos/Productos/18.jpg'
     },
     {
         ID:6,
         Nombre:'Taza de Stitch',
         Precio:45000,
-        categoria:'Tazas',
+        Categoria:'Tazas',
+        Tipo: 'disney',
         Descripcion:'Taza de cerámica con dibujos de Stitch',
-        Imagen:'../src/productos/19.jpg'
+        Imagen:'../src/Productos/Productos/19.jpg'
     },
     {
         ID:6,
         Nombre:'Taza de Elsa',
         Precio:45000,
-        categoria:'Tazas',
+        Categoria:'Tazas',
+        Tipo: 'disney',
         Descripcion:'Taza de cerámica con dibujos de Elsa',
-        Imagen:'../src/productos/20.jpg'
+        Imagen:'../src/Productos/Productos/20.jpg'
     }
 ]
