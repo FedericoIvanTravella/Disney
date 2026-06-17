0943516eca06c3c855407172ffc5bf5b048bfa8a
FedericoIvanTravella <Federicoitravella@gmail.com>
2026-06-17 18:15:17 -0300

    17/6

--- scripts/db.js
+++ scripts/db.js
@@ -158,5 +158,245 @@ export const DB=[
         categoria:'Tazas',
         Descripcion:'taza de ceramica con dibujos de elsa',
         Imagen:'../src/productos/20.jpg'
-    }
+    },
+    {
+    ID:21,
+    Nombre:'Botella térmica de Disney',
+    Precio:28000,
+    categoria:'Accesorios',
+    Descripcion:'botella térmica de acero inoxidable',
+    Imagen:'../src/productos/21.jpg'
+},
+{
+    ID:22,
+    Nombre:'Camiseta de Mickey Mouse',
+    Precio:32000,
+    categoria:'Ropa',
+    Descripcion:'camiseta de algodón estampada',
+    Imagen:'../src/productos/22.jpg'
+},
+{
+    ID:23,
+    Nombre:'Camiseta de Stitch',
+    Precio:34000,
+    categoria:'Ropa',
+    Descripcion:'camiseta cómoda con diseño de Stitch',
+    Imagen:'../src/productos/23.jpg'
+},
+{
+    ID:24,
+    Nombre:'Camiseta de Marvel',
+    Precio:36000,
+    categoria:'Ropa',
+    Descripcion:'camiseta con estampado de superhéroes',
+    Imagen:'../src/productos/24.jpg'
+},
+{
+    ID:25,
+    Nombre:'Sudadera de Disney',
+    Precio:62000,
+    categoria:'Ropa',
+    Descripcion:'sudadera abrigada con diseño Disney',
+    Imagen:'../src/productos/25.jpg'
+},
+{
+    ID:26,
+    Nombre:'Gorra de Mickey Mouse',
+    Precio:25000,
+    categoria:'Accesorios',
+    Descripcion:'gorra ajustable con bordado de Mickey',
+    Imagen:'../src/productos/26.jpg'
+},
+{
+    ID:27,
+    Nombre:'Gorra de Star Wars',
+    Precio:27000,
+    categoria:'Accesorios',
+    Descripcion:'gorra temática inspirada en Star Wars',
+    Imagen:'../src/productos/27.jpg'
+},
+{
+    ID:28,
+    Nombre:'Llavero de Stitch',
+    Precio:12000,
+    categoria:'Accesorios',
+    Descripcion:'llavero coleccionable de Stitch',
+    Imagen:'../src/productos/28.jpg'
+},
+{
+    ID:29,
+    Nombre:'Llavero de Mickey',
+    Precio:11000,
+    categoria:'Accesorios',
+    Descripcion:'llavero metálico de Mickey Mouse',
+    Imagen:'../src/productos/29.jpg'
+},
+{
+    ID:30,
+    Nombre:'Llavero de Grogu',
+    Precio:14000,
+    categoria:'Accesorios',
+    Descripcion:'llavero inspirado en Grogu',
+    Imagen:'../src/productos/30.jpg'
+},
+{
+    ID:31,
+    Nombre:'Cuaderno de Frozen',
+    Precio:18000,
+    categoria:'Escolar',
+    Descripcion:'cuaderno tapa dura de Frozen',
+    Imagen:'../src/productos/31.jpg'
+},
+{
+    ID:32,
+    Nombre:'Cuaderno de Disney Princess',
+    Precio:19000,
+    categoria:'Escolar',
+    Descripcion:'cuaderno ilustrado de princesas Disney',
+    Imagen:'../src/productos/32.jpg'
+},
+{
+    ID:33,
+    Nombre:'Set de lápices Disney',
+    Precio:15000,
+    categoria:'Escolar',
+    Descripcion:'set de lápices de colores Disney',
+    Imagen:'../src/productos/33.jpg'
+},
+{
+    ID:34,
+    Nombre:'Cartuchera de Stitch',
+    Precio:22000,
+    categoria:'Escolar',
+    Descripcion:'cartuchera con diseño de Stitch',
+    Imagen:'../src/productos/34.jpg'
+},
+{
+    ID:35,
+    Nombre:'Rompecabezas de Disney',
+    Precio:26000,
+    categoria:'Juegos',
+    Descripcion:'rompecabezas de 1000 piezas',
+    Imagen:'../src/productos/35.jpg'
+},
+{
+    ID:36,
+    Nombre:'Juego de mesa Disney Villains',
+    Precio:48000,
+    categoria:'Juegos',
+    Descripcion:'juego de estrategia de villanos Disney',
+    Imagen:'../src/productos/36.jpg'
+},
+{
+    ID:37,
+    Nombre:'LEGO Castillo de Disney',
+    Precio:185000,
+    categoria:'LEGO',
+    Descripcion:'set coleccionable del castillo Disney',
+    Imagen:'../src/productos/37.jpg'
+},
+{
+    ID:38,
+    Nombre:'LEGO Star Wars',
+    Precio:145000,
+    categoria:'LEGO',
+    Descripcion:'set de construcción Star Wars',
+    Imagen:'../src/productos/38.jpg'
+},
+{
+    ID:39,
+    Nombre:'LEGO Marvel',
+    Precio:132000,
+    categoria:'LEGO',
+    Descripcion:'set de construcción inspirado en Marvel',
+    Imagen:'../src/productos/39.jpg'
+},
+{
+    ID:40,
+    Nombre:'Funko Pop de Mickey Mouse',
+    Precio:35000,
+    categoria:'Coleccionables',
+    Descripcion:'figura Funko Pop de Mickey',
+    Imagen:'../src/productos/40.jpg'
+},
+{
+    ID:41,
+    Nombre:'Funko Pop de Elsa',
+    Precio:36000,
+    categoria:'Coleccionables',
+    Descripcion:'figura Funko Pop de Elsa',
+    Imagen:'../src/productos/41.jpg'
+},
+{
+    ID:42,
+    Nombre:'Funko Pop de Darth Vader',
+    Precio:39000,
+    categoria:'Coleccionables',
+    Descripcion:'figura Funko Pop de Darth Vader',
+    Imagen:'../src/productos/42.jpg'
+},
+{
+    ID:43,
+    Nombre:'Funko Pop de Spider-Man',
+    Precio:38000,
+    categoria:'Coleccionables',
+    Descripcion:'figura Funko Pop de Spider-Man',
+    Imagen:'../src/productos/43.jpg'
+},
+{
+    ID:44,
+    Nombre:'Reloj de Mickey Mouse',
+    Precio:58000,
+    categoria:'Accesorios',
+    Descripcion:'reloj analógico inspirado en Mickey',
+    Imagen:'../src/productos/44.jpg'
+},
+{
+    ID:45,
+    Nombre:'Almohadón de Stitch',
+    Precio:29000,
+    categoria:'Hogar',
+    Descripcion:'almohadón suave con diseño de Stitch',
+    Imagen:'../src/productos/45.jpg'
+},
+{
+    ID:46,
+    Nombre:'Manta de Frozen',
+    Precio:42000,
+    categoria:'Hogar',
+    Descripcion:'manta polar inspirada en Frozen',
+    Imagen:'../src/productos/46.jpg'
+},
+{
+    ID:47,
+    Nombre:'Auriculares de Disney',
+    Precio:68000,
+    categoria:'Tecnología',
+    Descripcion:'auriculares inalámbricos temáticos',
+    Imagen:'../src/productos/47.jpg'
+},
+{
+    ID:48,
+    Nombre:'Funda para celular de Stitch',
+    Precio:19000,
+    categoria:'Tecnología',
+    Descripcion:'funda protectora de Stitch',
+    Imagen:'../src/productos/48.jpg'
+},
+{
+    ID:49,
+    Nombre:'Bolsa reutilizable de Disney',
+    Precio:9000,
+    categoria:'Accesorios',
+    Descripcion:'bolsa ecológica con estampado Disney',
+    Imagen:'../src/productos/49.jpg'
+},
+{
+    ID:50,
+    Nombre:'Adorno decorativo del Castillo de Disney',
+    Precio:75000,
+    categoria:'Hogar',
+    Descripcion:'figura decorativa del castillo Disney',
+    Imagen:'../src/productos/50.jpg'
+}
 ]
